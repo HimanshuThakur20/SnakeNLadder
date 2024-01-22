25f689bfa4c8c8cf5d57552f2b228d37870e6ca1
Himanshu <himamshuthakur20@gmail.com>
2024-01-22 23:48:16 +0530

    Player 1 inititalized

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args){
+        int position=0;
+    Player p1 = new Player(position);
+
+        System.out.println("Position of the player 1: "+ p1.currentPosition );
 
     }
 }
