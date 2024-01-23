980c156c8fac0914d57e3b31d9e645a92bcdefb6
Himanshu <himamshuthakur20@gmail.com>
2024-01-23 12:30:18 +0530

    [Himanshu] Feature added: 2 player can playe

--- src/Main.java
+++ src/Main.java
@@ -6,29 +6,67 @@ public class Main {
         Main m1 = new Main();
 
         int position1=0;
-        Player p1 = new Player(position1);
+        Player p1 = new Player(position1,"Player 1");
+        Player p2 = new Player(position1,"Player 2");
+
         GameBoard g = new GameBoard();
 
-//        position1 += p1.dieValue;
-//        p1.currentPosition=position1;
-        System.out.println("Position of the player 1: "+ p1.currentPosition );
+        for(int i=0;;i++) {
+            if (p1.currentPosition == 100 || p2.currentPosition == 100) {
+                break;
+            } else if (i % 2 == 0) {
+                System.out.println("\nPlayer " + p1.playerName + " at play");
+                m1.play(p1, g);
+                System.out.println("Position of the player: "+ p1.currentPosition );
 
-        System.out.println("\nPress Enter to roll the die...");
-        sc.nextLine();
+            } else {
+                System.out.println("\nPlayer " + p2.playerName + " at play");
+                m1.play(p2, g);
+                System.out.println("Position of the player: "+ p2.currentPosition );
 
-        m1.dieRoll(p1) ;
-        while(p1.currentPosition==0){
+            }
+        }
 
+            if(p1.currentPosition==100){
+                System.out.println("Player 1 won!! Die count is" + p1.dieCount);
+            }
+            else{
+                System.out.println("Player 2 Won!! Die count is" + p2.dieCount);
+            }
+
+
+    }
+
+    public int dieRoll(Player p){
+        p.dieCount++;
+        return (p.dieValue= (int) (Math.random()*10%6) +1);
+    }
+
+    public void play(Player p1, GameBoard g){
+        Main m1 = new Main();
+        Scanner sc = new Scanner(System.in);
+
+            if(p1.currentPosition==0 ){
+                System.out.println("\nPress Enter to roll the die...");
+                sc.nextLine();
+                m1.dieRoll(p1) ;
+                if(p1.dieValue!=6) {
+                    System.out.println("\nYou need to  get a 6 to start the game. \n");
+                    return;
+                }
+            }
             if(p1.currentPosition==0 && p1.dieValue==6){
-                System.out.println("game started!!\n\n");
-                p1.currentPosition=0;
+                System.out.println("\n\nGame started!! for player "+ p1.playerName+"\n");
+                System.out.println("Position of the player: "+ p1.currentPosition );
 
-                while (p1.currentPosition!=100) {
+                p1.currentPosition=0;
+            }
 
                     System.out.println("\nPress Enter to roll the die...");
                     sc.nextLine();
 
                     m1.dieRoll(p1);
+
                     int updatePosition = p1.dieValue+p1.currentPosition;
 
                     if(g.ladders.contains(updatePosition)){
@@ -44,31 +82,12 @@ public class Main {
                     else {
                         if(updatePosition>100){
                             System.out.println("You should get exactly "+(100-p1.currentPosition)+" to win the game!");
-                            continue;
                         }
                         else {
                             p1.currentPosition = updatePosition;
                             System.out.println(p1);
                         }
                     }
-                }
-                if(p1.currentPosition==100){
-                    System.out.println("You won!!");
-                }
-
-
-            }
-            else {
-                System.out.println("\nPress Enter to roll the die and get a 6 to start the game.");
-                sc.nextLine();
-                m1.dieRoll(p1) ;
-            }
-        }
-
-
-    }
-
-    public int dieRoll(Player p){
-        return (p.dieValue= (int) (Math.random()*10%6) +1);
+                    return;
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,16 +1,21 @@
 public class Player {
+    String playerName;
     Integer currentPosition;
     Integer dieValue;
+    Integer dieCount;
 
-    Player(int position){
+    Player(int position,String name){
 
         this.currentPosition=position;
+        this.playerName=name;
+        this.dieCount=0;
     }
 
     @Override
     public String toString() {
-        return "Player 1 " +
-                "currentPosition=" + currentPosition +
+        return "\nPlayer " +
+                "playerName='" + playerName  +
+                ", currentPosition=" + currentPosition +
                 '\n';
     }
 }
